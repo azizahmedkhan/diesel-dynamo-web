eb397dfb1b2e33750a3f62b24546dbae985f62fb
azizahmedkhan <azizahmed.khan@gmail.com>
2025-04-11 17:56:25 +1200

    commenting booking and contact links

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -28,17 +28,20 @@ const HeroSection = () => {
             We provide all types of service and maintanace including engine rebuild, cam belt, suspension,and transmission repair and replacement.
           </p>
           <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
-            <Link to="/booking" className="btn-primary text-center">
-              Book a Service
-            </Link>
-            <Link to="/contact" className="btn-secondary text-center">
-              Contact Us
-            </Link>
+          <a 
+              href="tel:+642102583793"
+              className="btn-primary text-center"
+            >
+              <svg className="w-5 h-5 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+              </svg>
+              <span>Call Us</span>
+            </a>
             <a 
               href="https://www.google.com/maps?q=31c+Atkinson+avenue+Otahuhu+Auckland+New+Zealand&center=-36.9447,174.8444"
               target="_blank"
               rel="noopener noreferrer" 
-              className="btn-secondary text-center inline-flex items-center"
+              className="btn-primary text-center"
             >
               <svg className="w-5 h-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
@@ -46,15 +49,13 @@ const HeroSection = () => {
               </svg>
               <span className="flex items-center justify-center">Find Us</span>
             </a>
-            <a 
-              href="tel:+642102583793"
-              className="btn-secondary text-center inline-flex items-center"
-            >
-              <svg className="w-5 h-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-              </svg>
-              <span>Call Us</span>
-            </a>
+            {/* <Link to="/booking" className="btn-primary text-center">
+              Book a Service
+            </Link>
+            <Link to="/contact" className="btn-secondary text-center">
+              Contact Us
+            </Link> */}
+            
           </div>
         </div>
       </div>
